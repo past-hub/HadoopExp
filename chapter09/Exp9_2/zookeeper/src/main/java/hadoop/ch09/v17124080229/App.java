eb638e23bc5813606d5ccf21079ef1c83868d66e
linzuozhen <linzuozhena@163.com>
2019-12-08 19:05:10 +0800

    Hadoop实验9Java Api操作zookeeper

---
+++ chapter09/Exp9_2/zookeeper/src/main/java/hadoop/ch09/v17124080229/App.java
@@ -0,0 +1,13 @@
+package hadoop.ch09.v17124080229;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
