e6c9851cc826e7bf879d3f1d4112eadc949d2456
linzuozhen <linzuozhena@163.com>
2019-12-08 19:21:20 +0800

    hadoop实验6通过 Java API 访问 HBase

---
+++ chapter06/Exp6_3/HBaseDemo63/src/main/java/hadoop/ch06/v17124080229/App.java
@@ -0,0 +1,13 @@
+package hadoop.ch06.v17124080229;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ chapter06/Exp6_3/HBaseDemo63/src/test/java/hadoop/ch06/v17124080229/AppTest.java
@@ -0,0 +1,20 @@
+package hadoop.ch06.v17124080229;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
